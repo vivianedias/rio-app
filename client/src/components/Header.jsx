9bcbcf6e47abd0af0c066599dde1b3e4124aec0e
Viviane <vivianedias@protonmail.com>
2019-12-31 19:54:37 -0300

    feat(Header): Adds header style and navlinks to other pages

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+// import { withRouter } from 'react-router';
+// import _ from 'lodash';
+
+const Header = () => {
+
+	// constructor(props) {
+	// 	super(props);
+	// 	this.state = {
+	// 		isActive: false,
+	// 		isBurguerOpen: false,
+	// 	}
+	// }
+
+	// componentDidMount() {
+	// 	window.addEventListener('resize', _.throttle(this.setWindowWidth, 500), false);
+	// 	// this.props.dispatchScreenSize(document.documentElement.clientWidth);
+	// }
+
+	// componentWillUnmount() {
+	// 	window.removeEventListener('scroll', this.setWindowWidth, false);
+	// }
+
+	// setWindowWidth = () => {
+	// 	// this.props.dispatchScreenSize(document.documentElement.clientWidth);		
+	// }
+
+	// handleClick = (e) => {
+	// 	e.preventDefault();
+	// 	this.setState(prevState => ({ isBurguerOpen: !prevState.isBurguerOpen }));
+	// }
+
+		// const { auth: { isAuthenticated, user }, history } = this.props;
+		// const { isActive } = this.state;
+
+  return (
+    <nav
+      className="navbar"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+            alt="RIO Logo"
+          />
+        </a>
+        <button
+          type="button"
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </button>
+      </div>
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <NavLink to="/" className="navbar-item">
+            Home
+          </NavLink>
+          <button className="navbar-item">
+            Documentation
+          </button>
+          <div className="navbar-item has-dropdown is-hoverable">
+            <button className="navbar-link">
+              More
+            </button>
+            <div className="navbar-dropdown">
+              <button className="navbar-item">
+                About
+              </button>
+              <button className="navbar-item">
+                Jobs
+              </button>
+              <button className="navbar-item">
+                Contact
+              </button>
+              <hr className="navbar-divider" />
+              <button className="navbar-item">
+                Report an issue
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <NavLink to="/cadastro" className="button is-primary">
+                <strong>Cadastro</strong>
+              </NavLink>
+              <NavLink to="/entrar" className="button is-light">
+                Entrar
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+// const mapDispatchToProps = dispatch => ({
+// 	dispatchScreenSize: value => {
+// 		dispatch(setScreenSize(value));
+// 	},
+// 	logoutUser: (history) => {
+// 		dispatch(logoutUser(history));
+// 	}
+// })
+
+// const mapStateToProps = state => ({
+// 	auth: state.auth
+// })
+
+export default Header;
