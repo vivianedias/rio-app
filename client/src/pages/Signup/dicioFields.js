49ae552c2cedc90e5c3b54aaa99bfb013bf685b2
Viviane <vivianedias@protonmail.com>
2020-01-15 08:42:54 -0300

    feat(content): Add checkbox and radio content

---
+++ client/src/pages/Signup/dicioFields.js
@@ -0,0 +1,11 @@
+export const segment = ["Desenvolvedora de conteúdo", "Produtora de conteúdo", "Distribuidora de conteúdo", "Exibidora de conteúdo", "Empacotadora de conteúdo", "Programadora de conteúdo", "Outros"]
+
+export const actions = ["Streaming", "Cinema", "Publicidade", "Televisão", "Games", "Internet", "Outros"]
+
+export const functions = ["Produtor(a) de Conteúdo", "Roteirista", "Consultor(a) de Roteiro", "Pesquisador(a)", "Produtor(a) Executivo", "Controller", "Diretor(a) de Produção", "Assistente de Produção", "Platô", "Direto(a)", "Assistente de Direção", "StoryBoarder", "Continuista", "Produtor(a) de Elenco", "Preparador(a) de Elenco", "Diretor(a) de Arte", "Produtor(a) de Arte", "Cenógrafo(a)", "Produtor(a) de objetos", "Figurinista", "Caracterizador(a)", "Supervisor(a) de Efeitos Especiais", "Chefe de Maquinária", "Assistente de Máquinaria", "Chefe de Elétrica | Gaffer", "Eletricista de Pré-Light", "Diretor(a) de Fotografia", "Operador(a) de Câmera", "Assistente de Câmera", "DIT | Técnico(a) de Imagem Digital - TID", "GMA | Logger", "Still", "Operador(a) de Video Assist", "Técnico(a) de Som", "Assistente de Som", "Editor(a)", "Assistente de Edição", "Supervisor(a) de Pós-Produção", "Assistente de Pós-Produção", "Supervisor(a) de Efeitos Visuais", "Produtor(a) de Efeitos Visuais", "Editor(a) On-line", "Correção de Cor", "Editor(a) de Som", "Legendagem", "Supervisor(a) Musical", "Compositor(a) Musical", "Animador(a)"]
+
+export const color = ["Negra", "Branca", "Indigena", "Amarela", "Outros"]
+
+export const gender = ["Mulher Cis", "Mulher Trans", "Homem  Cis", "Homem Trans", "Queer", "Não Binário", "Outro"]
+
+export const identitySegments = ["Audiovisual Negro", "Audiovisual Indígena", "Audiovisual LGBTI+", "Audiovisual Feminista", "Audiovisual Queer", "Audiovisual Ambiental", "Minha empresa não é vocacionada para Conteúdo Identitário", "Outro"]
