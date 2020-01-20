1694fb69022f30b2d9c4438a918d520df2251711
Viviane <vivianedias@protonmail.com>
2020-01-20 11:37:27 -0300

    feat(datepicker): Add datepicker library

---
+++ client/src/components/DatePicker.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import DatePicker as Picker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const DatePicker = () => {
+  return (
+    <div>Datepicker</div>
+    // <Picker />
+    // <DatePicker
+    //   selected={getValues().birthday}
+    //   onChange={handleBirthday}
+    // />
+  )
+}
+
+export default DatePicker
