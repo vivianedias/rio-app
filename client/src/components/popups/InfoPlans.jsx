c9ef347fe7d9a27c533e48687aef9e40c42ef3ec
sahpds <sabrinapereira997@hotmail.com>
2020-02-06 17:25:56 -0300

    modal

---
+++ client/src/components/popups/InfoPlans.jsx
@@ -0,0 +1,35 @@
+import React, { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+import Flexbox from '../Flexbox'
+import seloPlans from '../../assets/selo.png'
+
+const InfoDelete = ({ toggleModalStatus }) => {
+
+    return (
+        <Fragment>
+            <Flexbox
+                width="100%"
+                center
+                margin={{
+                    top: 10,
+                    bottom: 20,
+                    left: 0,
+                    right: 0
+                }}
+            >
+                <a href="https://raio.agency/planos">
+                    <img src={seloPlans} />
+                </a>
+            </Flexbox>
+        </Fragment>
+    )
+}
+
+
+export default InfoDelete;
+
+const Title = styled.h1`
+  color: #FFFFFF;
+`;
