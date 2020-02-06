9f3e8116bfdae7a4271efde8eee98cd17d58518b
Viviane <vivianedias@protonmail.com>
2020-02-06 15:44:43 -0300

    feat: Remove default values user form

---
+++ client/build/precache-manifest.f59b8978c886d858feb12643dff1adcb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d5c07c4acd5515df8c2f7339ee1ff74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "90900e63bd9ebb3f4238",
+    "url": "/static/js/2.7a058716.chunk.js"
+  },
+  {
+    "revision": "bc83fdac270828ef8fbb1ca1755becb6",
+    "url": "/static/js/2.7a058716.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e4808ac99d0b27094e1a",
+    "url": "/static/js/main.ab00dac5.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
