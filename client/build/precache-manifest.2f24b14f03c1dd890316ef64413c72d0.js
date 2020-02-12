bd0f6128861b1e5e46e4aca89e3aa1a05a859b01
Viviane <vivianedias@protonmail.com>
2020-02-12 11:23:25 -0300

    Update client build for umbler

---
+++ client/build/precache-manifest.2f24b14f03c1dd890316ef64413c72d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed99160160fc312d232b859b382a6b5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cde78d87900201cf4c8",
+    "url": "/static/js/2.93dfea33.chunk.js"
+  },
+  {
+    "revision": "bc83fdac270828ef8fbb1ca1755becb6",
+    "url": "/static/js/2.93dfea33.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eb695e3dd42b1b3419c3",
+    "url": "/static/js/main.a1bec351.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  },
+  {
+    "revision": "345f5cb3d72b2a9c69cdde95f59dff4b",
+    "url": "/static/media/raio_bemvindo.345f5cb3.png"
+  },
+  {
+    "revision": "c1265b6ae07514d5b7a9f122b541344f",
+    "url": "/static/media/selo.c1265b6a.png"
+  }
+]);
